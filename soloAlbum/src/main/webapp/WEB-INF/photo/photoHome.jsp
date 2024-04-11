39958825561f754ab48b69c3fc1277c701225d03
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-04-11 23:29:56 +0900

    -- 구현 내역 1. 이미지 드래그 할 때 특정 구역에 접촉 시 이벤트 발생하도록 설정. 드래그라는 이벤트 알게 됨..... 바꿀지 말지 고민해야함 	- if((((clientRect.x > tX) && (clientRect.x < tX + tW)) && ((clientRect.y > tY) && (clientRect.y < tY + tH))) 	|| (((clientRect.left > tX) && (clientRect.left < tX + tW)) && ((clientRect.bottom > tY) && (clientRect.bottom < tY + tH))) 	|| (((clientRect.right > tX) && (clientRect.right < tX + tW)) && ((clientRect.top > tY) && (clientRect.top < tY + tH))) 	|| (((clientRect.right > tX) && (clientRect.right < tX + tW))&& ((clientRect.bottom > tY) && (clientRect.bottom < tY + tH)))) 2. 파일 서버에 넘길 리스트 생성, 파일 버튼 눌러도 초기화 되지 않도록 함 let fileList=[]; 	- 파일 추가시 이미지마다 순서대로 imgSmallBox num 값 부여 	- 파일 추가시 fileList 에 push 	- 삭제시 delete fileList[imgSmallBox.dataset.num] 3. 세로 방향으로 바꾸려다가 레이아웃 꼬여서 실패 4. 화면 앨범처럼 2페이지로 나누고 각 div 마다 클래스 붙임
    개발해야 할것 : 파일 태그 혹은 삭제시 dict 형태로 해당 구역 순서값과 파일 num 업데이트
    			페이지 넘기기 버튼 누르면 기존 mainContainer 넘어가고(display: none) 새로운 mainContainer
    생성
    			이렇게 생성된 값들 서버에 넘기기

--- soloAlbum/src/main/webapp/WEB-INF/photo/photoHome.jsp
+++ soloAlbum/src/main/webapp/WEB-INF/photo/photoHome.jsp
@@ -9,36 +9,41 @@
 </head>
 <body>
 	<form action="insertPhotos.do" method="post"
-		enctype="multipart/form-data">
-		<div id="photo_container">
+		enctype="multipart/form-data" id="insertPhotosForm" name="insertPhotosForm">
+		<div id="photoContainer">
 			<div id="imgBox">
 				<div>
 					<div onclick="handleClick()" id="photoButton">
-						<span>+</span>
+						<img src="./public/img/addIcon.png">
 					</div>
 					<input type="file" id="files" name="uploadFile" accept="image/*"
 						multiple onchange="fileChange(this)">
 				</div>
 			</div>
 			<div>
-			<div colspan="2" align="center">
-				<input type="submit" value="보내기" style="display:none;">
+				<div colspan="2" align="center">
+					<input type="submit" value="보내기" >
+				</div>
 			</div>
 		</div>
-		</div>
-		
-		<div id="main_container">
-				<div id="test_box">
-				
-				</div>
+
+		<div id="mainContainer">
+			<div id="left_page" class="pages">
+				<div id="AlbumBox1" class="AlbumBox"></div>
+				<div id="AlbumBox2" class="AlbumBox"></div>
+			</div>
+			<div id="rightPage" class="pages">
+				<div id="AlbumBox3" class="AlbumBox"></div>
+				<div id="AlbumBox4" class="AlbumBox"></div>
+			</div>
+			
+			
 		</div>
 	</form>
 </body>
 <script src="./public/js/main.js"></script>
 <script type="text/javascript">
-	
-	
-	
+let page = 0;
 	
 </script>
 </html>
--- soloAlbum/src/main/webapp/public/js/main.js
+++ soloAlbum/src/main/webapp/public/js/main.js
@@ -1,73 +1,110 @@
 let imgSwitch=0;
+let fileList=[];
+let num = 0;
+
 let startX=0, startY=0, offsetX=0, offsetY=0;
+const testImg = document.getElementById('testImg');
+const AlbumBox1 = document.getElementById('AlbumBox1');
+const testRect = AlbumBox1.getBoundingClientRect();
+
+let tW=testRect.width, tH=testRect.height, tY=testRect.y, tX=testRect.x
 
 const files = document.querySelector("#files");
 // input의 onchange에서 this를 인자로 넣었기 때문에 input은 files.addEventListener("change" (e))의 e.target 과 같다
+// 이미지 프리뷰 설정
 const fileChange = (input) => {
 	if (input.files && input.files[0]) {
 		const imgBox = document.querySelector('#imgBox');
-
+	
 		for(let i=0; i < input.files.length; i++){
 				var reader = new FileReader();
 				reader.onload = function(e) {
 					const img = document.createElement('img');
 					const imgSmallBox = document.createElement('div');
 					const newURL = URL.createObjectURL(input.files[i]);
-					const xButton = document.createElement('button');
-					
-					xButton.innerText = 'x';
+					const xButton = document.createElement('img');
+					xButton.src = './public/img/xIcon.png';
 					xButton.classList.add('xButton');	
+					
 					img.src = newURL;
 					img.classList.add('preview');
+					imgSmallBox.draggable="true";
 					imgSmallBox.classList.add('imgSmallBox');
+					imgSmallBox.setAttribute('data-num', String(num));
+					num += 1
+					console.log(imgSmallBox.dataset.num);
 					imgSmallBox.appendChild(img);
 					imgSmallBox.appendChild(xButton);
 					imgBox.appendChild(imgSmallBox);
-												
+
 // dropImage에서 parentNode 가 쓰이는데, imgSamllBox에 appendChild 된 후에 addEventLinstener 해준다
 					xButton.addEventListener("click", (e) => {
+					// x 버튼 클릭시 사진 삭제 이벤트
 						e.stopPropagation();
 						e.preventDefault();
+						// blob URL 삭제
 						URL.revokeObjectURL(xButton.parentNode.childNodes[0].src);
 						xButton.parentNode.parentNode.removeChild(xButton.parentNode);
+						// 파일리스트에서 삭제
+						delete fileList[imgSmallBox.dataset.num]
 					});
-					
+					imgSmallBox.addEventListener('dragstart', (e) => {
+						alert('drag test');
+					});
 					// img 드래그를 위한 코드
 					imgSmallBox.addEventListener("mousedown", function(e){
 					    // 드래그시 마우스 클릭 동작 방지
 					    e.preventDefault();
-					    // 마우스 현재 좌표
-					    startX = e.clientX;
-					    startY = e.clientY;
-					
-					    // 이동중인 마우스의 좌표값 업데이트
-					    offsetX = parseInt(imgSmallBox.style.left) - startX;
-					    offsetY = parseInt(imgSmallBox.style.top) - startY;
 					    
 					    // 이동중이거나 마우스 드래그 상태가 아닐때의 이벤트
 					    document.addEventListener("mousemove", startMove);
 					    document.addEventListener("mouseup", stopMove);
-						
+					    
 						function startMove(e) {
-						    // 이미지 드래그시 마우스 위치의 좌표로 이동
 						    imgSwitch=1;
+						    //imgSmallBox.style.boxShadow = '2px 2px 20px 10px grey'
 						    const clientRect = imgSmallBox.getBoundingClientRect();
-						    const mainContainer = document.querySelector('#main_container');
-						    mainContainer.appendChild(imgSmallBox);
+						    // 이미지 드래그시 마우스 위치의 좌표로 이동
+						    const mainContainer = document.querySelector('#mainContainer');
+						    //mainContainer.appendChild(imgSmallBox);
 						    imgSmallBox.style.position = 'absolute';
 						    imgSmallBox.style.left = e.clientX - (clientRect.width/2) + 'px';
 						    imgSmallBox.style.top = e.clientY - (clientRect.height/2) + 'px';
 						    imgSmallBox.style.transition='0s';
+						    // 구역 접촉 시 이벤트
+						    if((((clientRect.x > tX) && (clientRect.x < tX + tW)) && ((clientRect.y > tY) && (clientRect.y < tY + tH)))
+							|| (((clientRect.left > tX) && (clientRect.left < tX + tW)) && ((clientRect.bottom > tY) && (clientRect.bottom < tY + tH)))
+							|| (((clientRect.right > tX) && (clientRect.right < tX + tW)) && ((clientRect.top > tY) && (clientRect.top < tY + tH))) 
+							|| (((clientRect.right > tX) && (clientRect.right < tX + tW))&& ((clientRect.bottom > tY) && (clientRect.bottom < tY + tH)))){
+							    AlbumBox1.style.border='7px solid blue';
+						    } else {
+						   		 AlbumBox1.style.border='none'
+						    }
+						    
 						}
 						
 						function stopMove() {
-						    // imgSmallBox 원위치
-						    if(imgSwitch==1) {
-								imgBox.insertBefore(imgSmallBox, imgBox.childNodes[2]);						    
-							    imgSwitch=0
-						    }
+						//imgSmallBox.style.boxShadow = '2px 2px 10px 3px grey';
+						// imgSmallBox 원위치
+						const clientRect = imgSmallBox.getBoundingClientRect();
+						AlbumBox1.style.border='none'
+						    // 구역 안에서 놓았을 경우 이벤트
+						    if((((clientRect.x > tX) && (clientRect.x < tX + tW)) && ((clientRect.y > tY) && (clientRect.y < tY + tH)))
+							|| (((clientRect.left > tX) && (clientRect.left < tX + tW)) && ((clientRect.bottom > tY) && (clientRect.bottom < tY + tH)))
+							|| (((clientRect.right > tX) && (clientRect.right < tX + tW)) && ((clientRect.top > tY) && (clientRect.top < tY + tH))) 
+							|| (((clientRect.right > tX) && (clientRect.right < tX + tW))&& ((clientRect.bottom > tY) && (clientRect.bottom < tY + tH)))){
+								if(imgSwitch==1) {
+									AlbumBox1.appendChild(imgSmallBox);
+									imgSwitch=0;
+								}
+							// 구역 바깥에서 놓았을 경우 이벤트
+							} else {
+							    if(imgSwitch==1) {
+									imgBox.insertBefore(imgSmallBox, imgBox.childNodes[2]);						    
+								    imgSwitch=0;
+							    }							
+							}
 							imgSmallBox.style.position = 'relative';
-						    imgSmallBox.style.transition='0.15s';
 							imgSmallBox.style.left="0";
 							imgSmallBox.style.top="0";
 							
@@ -80,6 +117,15 @@ const fileChange = (input) => {
 				};
 				reader.readAsDataURL(input.files[i]);
 			}	
+			
+			// 리스트에 파일 추가
+			//console.log(new FormData(document.getElementById('insertPhotosForm')));
+			//console.log(new FormData(document.getElementById('insertPhotosForm')).getAll('uploadFile'));
+			const formData = new FormData(document.getElementById('insertPhotosForm')).getAll('uploadFile');
+			formData.forEach(file => {
+				fileList.push(file);
+			});
+			//console.log(fileList);
 	
 	  } else {
 	    document.getElementById('preview').src = "";
